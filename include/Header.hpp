94c74b4e6e3ea7fd184cddd38d552710fb5a8ccd
mhufflep <mhufflep@student.21-school.ru>
2022-06-30 22:54:34 +0500

    Added container for headers
    Co-authored-by: Wicca <WiccaCrow@users.noreply.github.com>

--- include/Header.hpp
+++ include/Header.hpp
@@ -27,10 +27,107 @@ class Header {
 
     virtual bool isValid(void) = 0;
 
-    bool parse(const std::string &line);
+    bool parse(const std::string &line, bool trimKey = false);
+
+    std::string toString(void);
 
     friend bool operator==(const Header &h1, const Header &h2);
     friend bool operator!=(const Header &h1, const Header &h2);
+};
+
+template<typename T>
+class Headers {
+public:
+    typedef typename std::map<uint32_t, T>::key_type key_type;  
+
+    typedef typename std::map<key_type, T>::pointer pointer;
+    typedef typename std::map<key_type, T>::const_pointer const_pointer;
+    typedef typename std::map<key_type, T>::reference reference;
+    typedef typename std::map<key_type, T>::const_reference const_reference;
+
+    typedef typename std::map<key_type, T>::iterator iterator;
+    typedef typename std::map<key_type, T>::const_iterator const_iterator;
+    typedef typename std::map<key_type, T>::reverse_iterator reverse_iterator;
+    typedef typename std::map<key_type, T>::const_reverse_iterator const_reverse_iterator;
+
+private:
+    std::map<key_type, T> _c;
+    
+public:
+    iterator begin(void) {
+        return _c.begin();
+    }
+
+    iterator end(void) {
+        return _c.end();
+    }
+
+    const_iterator cbegin(void) const {
+        return _c.begin();
+    }
+
+    const_iterator cend(void) const {
+        return _c.end();
+    }
+
+    reverse_iterator rbegin(void) {
+        return _c.rbegin();
+    }
+
+    reverse_iterator rend(void) {
+        return _c.rend();
+    }
+
+    const_reverse_iterator crbegin(void) const {
+        return _c.rbegin();
+    }
+
+    const_reverse_iterator crend(void) const {
+        return _c.rend();
+    }
+
+
+    bool has(HeaderCode key) {
+        return this->has(static_cast<key_type>(key));
+    }
+
+    bool has(key_type key) {
+        return (_c.find(key) != _c.end());
+    }
+
+    std::string &value(key_type key) {
+        return this->operator[](key).value;
+    }
+
+    std::string &value(HeaderCode key) {
+        return this->operator[](static_cast<key_type>(key)).value;
+    }
+
+    T &operator[](key_type key) {
+        T & ref = _c[key];
+        ref.hash = key;
+        return ref;
+    }
+
+    T &operator[](HeaderCode key) {
+        return this->operator[](static_cast<key_type>(key));
+    }
+
+    void insert(T val) {
+        _c.insert(std::make_pair<key_type, T>(val.hash, val));
+    }
+
+    void erase(key_type key) {
+        _c.erase(key);
+    }
+    
+    void erase(HeaderCode key) {
+        this->erase(static_cast<key_type>(key));
+    }
+
+    void clear(void) {
+        _c.clear();
+    }
 };
 
 } // namespace HTTP
--- src/Header.cpp
+++ src/Header.cpp
@@ -13,21 +13,29 @@ Header::Header(uint32_t hash) : hash(hash) {}
 Header::Header(uint32_t hash, const std::string &value) : hash(hash), value(value) {}
 
 bool
-Header::parse(const std::string &line) {
+Header::parse(const std::string &line, bool trimKey) {
     
-    // Log.debug << line << Log.endl;
+    // Log.debug() << line << Log.endl;
     size_t colonPos = line.find(':');
     if (colonPos == std::string::npos) {
         return false;
     }
 
     key = line.substr(0, colonPos);
+    if (trimKey) {
+        rtrim(key, SP);
+    }
     toLowerCase(key);
     value = line.substr(colonPos + 1);
-    trim(value, " \t\r\n");
+    trim(value, SP HTAB CR LF);
 
     hash = crc(key.c_str(), key.length());
     return true;
+}
+
+std::string
+Header::toString(void) {
+    return headerNames[hash] + ": " + value;
 }
 
 bool operator==(const Header &h1, const Header &h2) {
