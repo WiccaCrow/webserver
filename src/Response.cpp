1d2ab766d298264d246b80a6c14eae124c201e60
WiccaCrow <Selletrer@mail.ru>
2022-07-15 18:58:29 +0300

    added content-length for empty body

--- src/Response.cpp
+++ src/Response.cpp
@@ -347,7 +347,9 @@ int Response::makeResponseForFile(void) {
     addHeader(CONTENT_TYPE, getContentType(resourcePath));
     addHeader(ETAG, getEtagFile(resourcePath));
     addHeader(LAST_MODIFIED, Time::gmt(getModifiedTime(resourcePath)));
-
+    if (getBody().empty()) {
+        addHeader(CONTENT_LENGTH, "0");
+    }
     return 1;
 }
 
