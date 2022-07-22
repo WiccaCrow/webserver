325598aa6a80021a812ceb1d3e46ba6141cf6694
mhufflep <mhufflep@student.21-school.ru>
2022-07-22 19:57:47 +0500

    welcoming cgi script improved

---
+++ pages/site/welcome/welcome.py
@@ -0,0 +1,19 @@
+import os
+import sys
+from gtts import gTTS
+from io import BytesIO
+
+bytesN = int(os.environ["CONTENT_LENGTH"])
+msg = 'Welcome on webserv '
+
+if (bytesN != 0):
+    body = sys.stdin.read(bytesN)
+    body = body.replace('fname=', '')
+    msg += body + "!"
+
+tts = gTTS(text=msg, lang='en', slow=False)
+
+tts.save('./voice.mp3')
+
+print("Location: /welcome/welcome.html\n\n")
+
