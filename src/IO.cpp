6d5d513cf6af896bd053fed792ef316d6ef44484
mhufflep <mhufflep@student.21-school.ru>
2022-07-23 23:37:37 +0500

    Changed listening log

--- src/IO.cpp
+++ src/IO.cpp
@@ -193,7 +193,7 @@ IO::listen(const std::string &addr, std::size_t port) {
         return -1;
     }
 
-    Log.info() << "IO::listen [" << _fdr << "] -> " << "http://" << addr << ":" << port << Log.endl;
+    Log.info() << "Listening on http://" << addr << ":" << port << Log.endl;
     return _fdr;
 }
 
